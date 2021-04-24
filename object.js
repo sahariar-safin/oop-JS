5b26fea49fda3c7581c3918267010af26f0949ca
Shafin The Developer <sahariar350@gmail.com>
2021-04-24 23:02:08 +0600

    something is cooking

---
+++ object.js
@@ -0,0 +1,10 @@
+class Pendrive {
+    constructor(name, price) {
+        this.name = name,
+            this.price = price
+    }
+}
+
+const Avada = new Pendrive("avada", 123);
+
+console.log(Avada.name);
